896290670fc06a0781d597113a73be1b84506c02
Corentin <c.faure1996@gmail.com>
2019-06-30 18:22:24 +0200

    Presque fin

--- duel.js
+++ duel.js
@@ -1,184 +1,235 @@
-var exports = module.exports = {};
+var exports = (module.exports = {});
 const func = require("./includes/functionsDuel.js");
 const funcGlobal = require("./includes/functions.js");
 let percentWinAuthor = 50;
-
-exports.duel = function (msg, emojis, cooldownManager, commandPrefix, client, commandsList, serverMembers) {
-
-    if (msg.channel.name === "botcommands") {
-        if (funcGlobal.isCommand(msg.content, commandPrefix, commandsList)) {
-            funcGlobal.setCooldown(msg.content, cooldownManager);
-        }
-        if (msg.content.split(' ')[0] === commandPrefix + "link" && msg.content.split(' ')[1].length != 0) {
-            func.linkChar(msg.content.split(' ')[1])
-                .then(message => {
-                    msg.channel.send(message);
-                });
-        }
+let callbacks = 0;
+exports.duel = function(
+  msg,
+  emojis,
+  cooldownManager,
+  commandPrefix,
+  client,
+  commandsList,
+  serverMembers
+) {
+  if (msg.channel.name === "botcommands") {
+    if (funcGlobal.isCommand(msg.content, commandPrefix, commandsList)) {
+      funcGlobal.setCooldown(msg.content, cooldownManager);
     }
-    if (msg.channel.name === "duel") {
+    if (
+      msg.content.split(" ")[0] === commandPrefix + "link" &&
+      msg.content.split(" ")[1].length != 0
+    ) {
+      func.linkChar(msg.content.split(" ")[1], msg.author.id, message => {
+        msg.channel.send(message);
+      });
+    }
+  }
+  if (msg.channel.name === "duel") {
+    if (funcGlobal.isCommand(msg.content, commandPrefix, commandsList)) {
+      funcGlobal.setCooldown(msg.content, cooldownManager);
+    }
+    if (
+      msg.content.split(" ")[0] === commandPrefix + "duel" &&
+      msg.content.split(" ")[1].match(/[\\<>@!\d]/g)
+    ) {
+      let target = msg.content.split(" ")[1];
+      let targetId = target.replace(/[\\<>@!]/g, "");
 
-        if (funcGlobal.isCommand(msg.content, commandPrefix, commandsList)) {
-            funcGlobal.setCooldown(msg.content, cooldownManager);
-        }
-        if (msg.content.split(' ')[0] === commandPrefix + "duel" && msg.content.split(' ')[1].match(/[\\<>@!\d]/g)) {
-            let targetId = msg.content.split(' ')[1].replace(/[\\<>@!]/g, "");
-            let opponentChar = null;
-            let authorChar = func.getChar(msg.author.id);
-            if (serverMembers.get(targetId) != undefined) {
-                opponentChar = func.getChar(targetId);
-            }
-            else {
-                msg.channel.send("L'utilisateur ciblé n'est pas sur le serveur Discord.");
-                return;
-            }
-
-            if (authorChar != null && opponentChar != null && authorChar.level === opponentChar.level) {
-
-                let diffIlvl = Math.ceil(Math.abs(authorChar.ilvl - opponentChar.ilvl) / 2);
-                if (authorChar.ilvl > opponentChar.ilvl) {
+      if (serverMembers.get(targetId) != undefined) {
+        func.getChar(targetId, opponentChar => {
+          func.getChar(msg.author.id, authorChar => {
+            if (
+              authorChar != null &&
+              opponentChar != null &&
+              authorChar.level === opponentChar.level
+            ) {
+              func.getArmorType(authorChar.class, armorAuthor => {
+                func.getArmorType(opponentChar.class, armorOpponent => {
+                  let diffIlvl = Math.ceil(
+                    Math.abs(authorChar.ilvl - opponentChar.ilvl) / 2
+                  );
+                  if (authorChar.ilvl > opponentChar.ilvl) {
                     percentWinAuthor -= diffIlvl;
-                }
-                else {
+                  } else {
                     percentWinAuthor += diffIlvl;
-                }
+                  }
 
-                let armorAuthor = null;
-                func.getArmorType(authorChar.class, (res) => {
-                    armorAuthor = res;
-                });
-
-                let armorOpponent = null;
-                func.getArmorType(opponentChar.class, (res) => {
-                    armorOpponent = res;
-                });
-                if (armorAuthor != null && armorOpponent != null) {
-                    switch (armorAuthor) {
-                        case 'T':
-                            if (armorOpponent === 'C') {
-                                func.fleeAway(authorChar.class, (message) => {
-                                    message = message.replace('{X}', authorChar.name);
-                                    message = message.replace('{Y}', opponentChar.name);
-                                    msg.channel.send(message);
-                                    return;
-                                });
-                            }
-                            else if (armorOpponent === 'P') {
-                                func.fleeAway(opponentChar.class, (message) => {
-                                    message = message.replace('{X}', opponentChar.name);
-                                    message = message.replace('{Y}', authorChar.name);
-                                    msg.channel.send(message);
-                                    return;
-                                });
-                            }
-                            break;
-                        case 'C':
-                            if (armorOpponent === 'M') {
-                                func.fleeAway(authorChar.class, (message) => {
-                                    message = message.replace('{X}', authorChar.name);
-                                    message = message.replace('{Y}', opponentChar.name);
-                                    msg.channel.send(message);
-                                    return;
-                                });
-                            }
-                            else if (armorOpponent === 'T') {
-                                func.fleeAway(opponentChar.class, (message) => {
-                                    message = message.replace('{X}', opponentChar.name);
-                                    message = message.replace('{Y}', authorChar.name);
-                                    msg.channel.send(message);
-                                    return;
-                                });
-                            }
-                            break;
-                        case 'M':
-                            if (armorOpponent === 'P') {
-                                func.fleeAway(authorChar.class, (message) => {
-                                    message = message.replace('{X}', authorChar.name);
-                                    message = message.replace('{Y}', opponentChar.name);
-                                    msg.channel.send(message);
-                                    return;
-                                });
-                            }
-                            else if (armorOpponent === 'C') {
-                                func.fleeAway(opponentChar.class, (message) => {
-                                    message = message.replace('{X}', opponentChar.name);
-                                    message = message.replace('{Y}', authorChar.name);
-                                    msg.channel.send(message);
-                                    return;
-                                });
-                            }
-                            break;
-                        case 'P':
-                            if (armorOpponent === 'T') {
-                                func.fleeAway(authorChar.class, (message) => {
-                                    message = message.replace('{X}', authorChar.name);
-                                    message = message.replace('{Y}', opponentChar.name);
-                                    msg.channel.send(message);
-                                    return;
-                                });
-                            }
-                            else if (armorOpponent === 'M') {
-                                func.fleeAway(opponentChar.class, (message) => {
-                                    message = message.replace('{X}', opponentChar.name);
-                                    message = message.replace('{Y}', authorChar.name);
-                                    msg.channel.send(message);
-                                    return;
-                                });
-                            }
-                            break;
+                  if (armorAuthor != null && armorOpponent != null) {
+                    switch (armorAuthor.typeName) {
+                      case "T":
+                        if (armorOpponent.typeName === "C") {
+                          func.fleeAway(authorChar.class, message => {
+                            message = message.replace("{X}", msg.author.toString());
+                            message = message.replace("{Y}", target);
+                            msg.channel.send(message);
+                          });
+                          return;
+                        } else if (armorOpponent.typeName === "P") {
+                          func.fleeAway(opponentChar.class, message => {
+                            message = message.replace("{X}", target);
+                            message = message.replace("{Y}", msg.author.toString());
+                            msg.channel.send(message);
+                          });
+                          return;
+                        }
+                        break;
+                      case "C":
+                        if (armorOpponent.typeName === "M") {
+                          func.fleeAway(authorChar.class, message => {
+                            message = message.replace("{X}", msg.author.toString());
+                            message = message.replace("{Y}", target);
+                            msg.channel.send(message);
+                          });
+                          return;
+                        } else if (armorOpponent.typeName === "T") {
+                          func.fleeAway(opponentChar.class, message => {
+                            message = message.replace("{X}", target);
+                            message = message.replace("{Y}", msg.author.toString());
+                            msg.channel.send(message);
+                          });
+                          return;
+                        }
+                        break;
+                      case "M":
+                        if (armorOpponent.typeName === "P") {
+                          func.fleeAway(authorChar.class, message => {
+                            message = message.replace("{X}", msg.author.toString());
+                            message = message.replace("{Y}", target);
+                            msg.channel.send(message);
+                          });
+                          return;
+                        } else if (armorOpponent.typeName === "C") {
+                          func.fleeAway(opponentChar.class, message => {
+                            message = message.replace("{X}", target);
+                            message = message.replace("{Y}", msg.author.toString());
+                            msg.channel.send(message);
+                          });
+                          return;
+                        }
+                        break;
+                      case "P":
+                        if (armorOpponent.typeName === "T") {
+                          func.fleeAway(authorChar.class, message => {
+                            message = message.replace("{X}", msg.author.toString());
+                            message = message.replace("{Y}", target);
+                            msg.channel.send(message);
+                          });
+                          return;
+                        } else if (armorOpponent.typeName === "M") {
+                          func.fleeAway(opponentChar.class, message => {
+                            message = message.replace("{X}", target);
+                            message = message.replace("{Y}", msg.author.toString());
+                            msg.channel.send(message);
+                          });
+                          return;
+                        }
+                        break;
                     }
-                }
+                    return;
+                  }
 
-                let win = Math.floor(Math.random() * Math.floor(100));
-                if (win < percentWinAuthor) {
+                  let win = Math.floor(Math.random() * Math.floor(100));
+                  if (win < percentWinAuthor) {
                     //Win de l'auteur
-                    func.winMessage(authorChar.name, opponentChar.name, (message) => {
+                    func.winMessage(
+                        msg.author.toString(),
+                        target,
+                      message => {
                         msg.channel.send(message);
-                        msg.channel.send("("+msg.author.toString()+" avait "+percentWinAuthor+"% de chances de l'emporter.)");
-                    });
+                        msg.channel.send(
+                          "(" +
+                            msg.author.toString() +
+                            " avait " +
+                            percentWinAuthor +
+                            "% de chances de l'emporter.)"
+                        );
+                      }
+                    );
+                    percentWinAuthor = 50;
                     return;
-                }
-                else if (win > percentWinAuthor) {
+                  } else if (win > percentWinAuthor) {
                     //win de l'opposant
-                    func.winMessage(opponentChar.name, authorChar.name, (message) => {
+                    func.winMessage(
+                      target,
+                      msg.author.toString(),
+                      message => {
                         msg.channel.send(message);
-                        msg.channel.send("("+msg.author.toString()+" avait "+percentWinAuthor+"% de chances de l'emporter.)");
-                    });
+                        msg.channel.send(
+                          "(" +
+                            msg.author.toString() +
+                            " avait " +
+                            percentWinAuthor +
+                            "% de chances de l'emporter.)"
+                        );
+                      }
+                    );
+                    percentWinAuthor = 50;
                     return;
-                }
-                else {
+                  } else {
                     //égalité
-                    msg.channel.send("C'est une étonnante égalité entre " + authorChar.name + " et " + opponentChar.name + " ! :o");
-                    msg.channel.send("(Ils avaient chacun"+percentWinAuthor+"% de chances de l'emporter.)");
+                    msg.channel.send(
+                      "C'est une étonnante égalité entre " +
+                      msg.author.toString() +
+                        " et " +
+                        target +
+                        " ! :o"
+                    );
+                    msg.channel.send(
+                      "(Ils avaient chacun" +
+                        percentWinAuthor +
+                        "% de chances de l'emporter.)"
+                    );
+                    percentWinAuthor = 50;
                     return;
-                }
-
-
+                  }
+                });
+              });
+            } else {
+              //Faire duel 100% random si l'auteur de la commande n'a pas de perso link OU que l'utilisateur tag en paramètre n'en a pas OU que les persos n'ont pas le même level
+              let win = Math.floor(Math.random() * Math.floor(100));
+              if (win < 50) {
+                //Win de l'auteur
+                func.winMessage(
+                  msg.author.toString(),
+                  "<@" + targetId + ">",
+                  message => {
+                    msg.channel.send(message);
+                  }
+                );
+                return;
+              } else if (win > 50) {
+                //win de l'opposant
+                func.winMessage(
+                  "<@" + targetId + ">",
+                  msg.author.toString(),
+                  message => {
+                    msg.channel.send(message);
+                  }
+                );
+                return;
+              } else {
+                //égalité
+                msg.channel.send(
+                  "C'est une étonnante égalité entre " +
+                    msg.author.toString() +
+                    " et " +
+                    "<@" +
+                    targetId +
+                    ">" +
+                    " ! :o"
+                );
+                return;
+              }
             }
-            else {
-                //Faire duel 100% random si l'auteur de la commande n'a pas de perso link OU que l'utilisateur tag en paramètre n'en a pas OU que les persos n'ont pas le même level
-                let win = Math.floor(Math.random() * Math.floor(100));
-                if (win < 50) {
-                    //Win de l'auteur
-                    func.winMessage(msg.author.toString(), "<@"+targetId+">", (message) => {
-                        msg.channel.send(message);
-                    });
-                    return;
-                }
-                else if (win > 50) {
-                    //win de l'opposant
-                    func.winMessage("<@"+targetId+">", msg.author.toString(), (message) => {
-                        msg.channel.send(message);
-                    });
-                    return;
-                }
-                else {
-                    //égalité
-                    msg.channel.send("C'est une étonnante égalité entre " + msg.author.toString() + " et " + "<@"+targetId+">" + " ! :o");
-                    return;
-                }
-            }
-        }
-        
+          });
+        });
+      } else {
+        msg.channel.send(
+          "L'utilisateur ciblé n'est pas sur le serveur Discord."
+        );
+        return;
+      }
     }
-}
+  }
+};
--- includes/functionsDuel.js
+++ includes/functionsDuel.js
@@ -1,134 +1,164 @@
 let connection = require("./dbHandler.js");
 let AuthDetails = require("./auth.json");
-const blizzard = require('blizzard.js').initialize({
-    key: AuthDetails.blizzardClientId,
-    secret: AuthDetails.blizzardClientSecret
+const blizzard = require("blizzard.js").initialize({
+  key: AuthDetails.blizzardClientId,
+  secret: AuthDetails.blizzardClientSecret
 });
 
-
 module.exports = {
-    CharFromAPI: {
-        class: 0,
-        level: 0,
-        ilvl: 0,
-        server: '',
-        name: ''
-    },
-    /*
+  /*
+    Fonction getCharFromAPI
+    R : Récupère le personnage sur l'API WoW dont le Pseudo-Serveur est passé en paramètre
+    E : Nom du personnage, serveur du personnage
+    S : un objet
+    */
+  getCharFromAPI: function(charName, callback) {
+    let char = charName.split("-")[0];
+    let server = charName.split("-")[1];
+
+    blizzard.getApplicationToken().then(response => {
+      blizzard.defaults.token = response.data.access_token;
+      blizzard.wow
+        .character(["items"], {
+          origin: "eu",
+          realm: server.toLowerCase(),
+          name: char.toLowerCase(),
+          locale: "fr_FR"
+        })
+        .then(response => {
+          let charReturn = {
+            class: response.data.class,
+            level: response.data.level,
+            ilvl: response.data.items.averageItemLevel,
+            server: response.data.realm,
+            name: response.data.name
+          };
+          return callback(charReturn);
+        })
+        .catch(err => console.log(err));
+    });
+  },
+  /*
     Fonction getChar
-    R : Récupérer le personnage de l'utilisateur en base
+    R : Récupérer le personnage de l'utilisateur en base + avec l'API
     E : ID de l'utilisateur
     S : objet CharFromAPI
     */
-    getChar: function (authorId) {
+  getChar: function(authorId, callback) {
+    let sql = "SELECT * FROM linkedChar WHERE idDiscord=?";
+    connection.query(sql, authorId, (err, res) => {
+      if (typeof res[0] != "undefined") {
+        this.getCharFromAPI(
+          res[0].charName + "-" + res[0].charRealm,
+          charObj => {
+            return callback(charObj);
+          }
+        );
+      }
+      else {
+          return callback(null);
+      }
+    });
+  },
 
-    },
-
-    /*
+  /*
     Fonction linkChar
     R : Permet à un utilisateur de lier son personnage à son pseudo Discord en base
     E : Pseudo-Serveur en argument à la commande
     S : Vide
     */
-    linkChar: function (fullCharName, authorId) {
+  linkChar: function(fullCharName, authorId, callback) {
+    this.getCharFromAPI(fullCharName, charObj => {
+      let sql = "SELECT * FROM linkedChar WHERE idDiscord=?";
+      connection.query(sql, authorId, (err, res) => {
+        if (typeof res[0] != "undefined")
+          return callback("Vous avez déjà un personnage de lié.");
 
-        this.getCharFromAPI(fullCharName)
-            .then(charObj => {
-                if (charObj.name === '') {
-                    return "Personnage non trouvé. Vérifiez la commande.";
-                }
-                else {
-                    let sql = "INSERT INTO linkedChar (idDiscord, charName, charRealm, charIlvl, charClass, charLevel) VALUES (?, ?, ?, ?, ?, ?)";
-                    connection.query(sql, [authorId, charObj.name, charObj.server, charObj.ilvl, charObj.class, charObj.level], (err) => {
-                        if (err) console.log(err);
-                        return "Vous avez lié votre profil Discord à votre personnage " + fullCharName;
-                    });
-                }
-            });
+        if (charObj.name === "") {
+          return callback("Personnage non trouvé. Vérifiez la commande.");
+        } else {
+          sql =
+            "INSERT INTO linkedChar (idDiscord, charName, charRealm, charClass) VALUES (?, ?, ?, ?)";
+          connection.query(
+            sql,
+            [authorId, charObj.name, charObj.server, charObj.class],
+            err => {
+              if (err) console.log(err);
+              return callback(
+                "Vous avez lié votre profil Discord à votre personnage " +
+                  fullCharName
+              );
+            }
+          );
+        }
+      });
+    });
+  },
 
-    },
-
-    /*
-    Fonction getCharFromAPI
-    R : Récupère le personnage sur l'API WoW dont le Pseudo-Serveur est passé en paramètre
-    E : Nom du personnage, serveur du personnage
-    S : un objet CharFromAPI
-    */
-    getCharFromAPI: function (charName) {
-        let char = charName.split('-')[0];
-        let server = charName.split('-')[1];
-        let charReturn = new this.CharFromAPI;
-        blizzard.wow.character(['items'], { origin: 'eu', realm: server.toLowerCase(), name: char.toLowerCase(), locale: 'fr_FR' })
-            .then(response => {
-                charReturn.level = response.data.level;
-                charReturn.ilvl = response.data.items.averageItemLevel;
-                charReturn.server = response.data.realm;
-                charReturn.name = response.data.name;
-                charReturn.class = response.data.class;
-                return charReturn;
-            }); //faire un .then à l'appel de cette fonction
-    },
-
-    /*
+  /*
     Fonction getArmorType
     R : Récupère le type d'armure du personnage dans la BDD selon la classe
     E : La classe du personnage
     S : Le type d'armure
     */
-    getArmorType: function (charClass, callback) {
-        let sql = "SELECT armorType FROM classArmorType WHERE class=?";
-        connection.query(sql, [charClass], (err, res) => {
-            if (err) console.log(err);
-            if (typeof res[0] != 'undefined') {
-                return callback(res[0]);
-            }
-        });
-    },
+  getArmorType: function(charClass, callback) {
+    let sql = "SELECT at.typeName FROM classArmorType AS cat INNER JOIN armorType AS at ON cat.armorType = at.id WHERE cat.class=?";
+    connection.query(sql, [charClass], (err, res) => {
+      if (err) console.log(err);
+      if (typeof res[0] != "undefined") {
+        return callback(res[0]);
+      }
+    });
+  },
 
-    /*
+  /*
     Fonction fleeAway
     R : Gère la fuite du duel en cas de malus
     E : Classes du fuyard
     S : Message textuel si il y a une fuite
     */
-    fleeAway: function (classFlee, callback) {
-        let flee = Math.floor(Math.random() * Math.floor(100));
-        if (flee <= 15) {
-            let sql = "SELECT phrase FROM fleeCatchPhrases WHERE classId=?";
-            connection.query(sql, [classFlee], (err, res) => {
-                if (err) console.log(err);
-                if (typeof res[0] != 'undefined') {
-                    return callback(res[0].phrase);
-                }
-            });
+  fleeAway: function(classFlee, callback) {
+    let flee = Math.floor(Math.random() * Math.floor(100));
+    
+    if (flee < 15) {
+      let sql = "SELECT phrase FROM fleeCatchPhrases WHERE classId=?";
+      connection.query(sql, [classFlee], (err, res) => {
+        if (err) console.log(err);
+        if (typeof res[0] != "undefined") {
+          return callback(res[0].phrase);
         }
+      });
+    }
+  },
 
-    },
-
-    /*
+  /*
     Fonction winMessage
     R : Récupère un message de victoire aléatoire et le formate
     E : Noms des participants
     S : Message textuel
     */
-    winMessage: function (winner, looser, callback) {
-        let sql = "SELECT COUNT(phrase) FROM winPhrases";
-        let message = "";
-        connection.query(sql, (err, res) => {
-            if (err) console.log(err);
-            if (typeof res != 'undefined') {
-                sql = "SELECT phrase FROM winPhrases WHERE id=?";
-                let rand = Math.floor(Math.random() * Math.floor(res[0]["COUNT(phrase)"]));
-                connection.query(sql, [rand], (err, res) => {
-                    if (err) console.log(err);
-                    if (typeof res != 'undefined') {
-                        message = res[0].phrase.replace('{X}', winner);
-                        message = message.replace('{Y}', looser);
-                        return callback(message);
-                    }
-                });
-            }
+  winMessage: function(winner, looser, callback) {
+    let sql = "SELECT COUNT(phrase) FROM winPhrases";
+    let message = "";
+    connection.query(sql, (err, res) => {
+      if (err) console.log(err);
+      if (typeof res != "undefined") {
+        sql = "SELECT phrase FROM winPhrases WHERE id=?";
+        let rand = Math.floor(
+          Math.random() * Math.floor(res[0]["COUNT(phrase)"]+1)
+        );
+        if(rand === 0) {
+            rand = 1;
+        }
+        connection.query(sql, [rand], (err, res) => {
+          if (err) console.log(err);
+          if (typeof res != "undefined") {
+            message = res[0].phrase.replace("{X}", winner);
+            message = message.replace("{Y}", looser);
+            return callback(message);
+          }
         });
-    }
-
+      }
+    });
+  }
 };

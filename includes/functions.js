c3203d2a9610359446495fd1c7bd693d1b77171c
FAURE Corentin -EXT <corentin.faure-ext@alstomgroup.com>
2019-07-01 14:19:36 +0200

    update setCooldown

--- includes/functions.js
+++ includes/functions.js
@@ -1,19 +1,17 @@
 module.exports = {
-	isCommand: function(message, commandPrefix, commandsList) {
+	isCommand: function (message, commandPrefix, commandsList) {
 		commandsList.forEach(cmd => {
-			if(message.split(' ')[0] === commandPrefix+cmd) {
+			if (message.split(' ')[0] === commandPrefix + cmd) {
 				return true;
 			}
 		});
 		return false;
 	},
-	setCooldown: function(message, cooldownManager) {
-		if(isMessage(message)) {
-			if (cooldownManager.canUse(message.split(' ')[0])) {
-                cooldownManager.touch(message.split(' ')[0]);
-            } else {
-                return;
-            }
+	setCooldown: function (message, cooldownManager) {
+		if (cooldownManager.canUse(message.split(' ')[0])) {
+			cooldownManager.touch(message.split(' ')[0]);
+		} else {
+			return;
 		}
 	}
 };

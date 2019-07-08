27869f3d1b2cc847a42a0e6f071f8ce8d8e7f089
FAURE Corentin -EXT <corentin.faure-ext@alstomgroup.com>
2019-07-08 13:57:28 +0200

    TEst again fk im bored

--- duel.js
+++ duel.js
@@ -39,7 +39,7 @@ exports.duel = async function (
         });
         return;
       }
-      if (msg.content === "fhglsiergfumzqerjghu") {
+      if (msg.content === commandPrefix + "linkchars") {
         console.log(msg.content);
         /*func.getAllDbChar((chars) => {
           console.log(chars);

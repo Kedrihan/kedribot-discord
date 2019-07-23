cc402b0ea64fd5493330d7d866c53a75fb7f7fb1
FAURE Corentin -EXT <corentin.faure-ext@alstomgroup.com>
2019-07-23 14:53:20 +0200

    ok

--- mp.js
+++ mp.js
@@ -1,7 +1,7 @@
 var exports = (module.exports = {});
 
-exports.mp = async function (message, cruellaServerMembers) {
-    if(message.guild === null && message.author.username === "Kedrihan" && message.content === "123mp") {
+exports.mp = function (message, cruellaServerMembers) {
+    if(message.guild === null && message.author.username === "Kedrihan") {
         
         message.channel.send("Ok.");
         message.channel.recipient = cruellaServerMembers.get("288659667268141056");
@@ -13,6 +13,6 @@ exports.mp = async function (message, cruellaServerMembers) {
 
         message.channel.recipient = cruellaServerMembers.get("116893981526196227");
 
-        message.channel.send(message);
+        message.channel.send("_Transmission entrante de la part de "+message.author.username+"_ \n"+message);
     }
 }

5aeeb368d177105312c7de6d1411daaf122f91ed
FAURE Corentin -EXT <corentin.faure-ext@alstomgroup.com>
2019-07-23 16:36:28 +0200

    Yes

--- mp.js
+++ mp.js
@@ -7,12 +7,4 @@ exports.mp = function (message, cruellaServerMembers) {
         userToSend = cruellaServerMembers.get("288659667268141056");
         userToSend.send("_Transmission entrante de la part de Kedrihan_ \n"+message)
     }
-    if(message.guild === null && !message.author.username === "Kedrihan") {
-
-        message.channel.send("Ton message va être transféré à Kedrihan :)");
-
-        kedrihan = cruellaServerMembers.get("116893981526196227");
-
-        kedrihan.send("_Transmission entrante de la part de "+message.author.username+"_ \n"+message);
-    }
 }

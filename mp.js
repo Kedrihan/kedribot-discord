be62267ccc894394364c17a0edcfc30d7a15cbc4
FAURE Corentin -EXT <corentin.faure-ext@alstomgroup.com>
2019-07-29 11:08:07 +0200

    ça va le faire

--- mp.js
+++ mp.js
@@ -8,10 +8,8 @@ exports.mp = function (message, cruellaServerMembers) {
             if (typeof res[0] != "undefined") {
                 res.forEach(id => {
                     userToSend = cruellaServerMembers.get(id.id);
-                    userToSend.send("_Transmission entrante de la part de Kedrihan_ \n" + message)
-                    .then(msg => {
-                        message.channel.send("Message envoyé à "+userToSend.user.username);
-                    });
+                    userToSend.send("_Transmission entrante de la part de Kedrihan_ \n" + message);
+                    message.channel.send("Message envoyé à "+userToSend.user.username);
                 });
             }
         });

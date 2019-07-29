3c5bec97a0f6cbcf240a39820b4966413118f560
FAURE Corentin -EXT <corentin.faure-ext@alstomgroup.com>
2019-07-29 10:59:54 +0200

    Ok.

--- mp.js
+++ mp.js
@@ -3,14 +3,15 @@ let connection = require("./includes/dbHandler.js");
 
 exports.mp = function (message, cruellaServerMembers) {
     if (message.guild === null && message.author.username === "Kedrihan") {
-
-        message.channel.send("Ok.");
         let sql = "SELECT * FROM mp";
         connection.query(sql, (err, res) => {
             if (typeof res[0] != "undefined") {
                 res.forEach(id => {
                     userToSend = cruellaServerMembers.get(id.id);
                     userToSend.send("_Transmission entrante de la part de Kedrihan_ \n" + message)
+                    .then(msg => {
+                        message.channel.send("Message envoyé à "+userToSend.username);
+                    });
                 });
             }
         });
